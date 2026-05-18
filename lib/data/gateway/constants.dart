edb81d53a8bce5603a78ca0a77a107b09adf8f39
Ptyg <nawkking@gmail.com>
2026-05-18 17:47:13 +0200

    chore: remove unused constant

--- lib/data/gateway/constants.dart
+++ lib/data/gateway/constants.dart
@@ -3,6 +3,5 @@ const String currencyEUR = 'eur';
 const String currencyUAH = 'uah';
 const String currencyJPY = 'jpy';
 const String sparkLineIsTrue = 'true';
-const String sparkLineIsFalse = 'false';
 const String order = 'market_cap_desc';
 const int perPage100 = 100;
